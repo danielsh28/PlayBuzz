834ce92de59cae2cd31a27e65bd71f4231e3b8e8
danielsh28 <danielshely1@gmail.com>
2019-07-07 20:17:33 +0300

    Updates

---
+++ precache-manifest.8496c781879e782c3a7904c99482961d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0f5c6545779a652a6134f50eab8ce8",
+    "url": "/playBuzz/index.html"
+  },
+  {
+    "revision": "e5d52d4f8812e5faf075",
+    "url": "/playBuzz/static/css/main.57034392.chunk.css"
+  },
+  {
+    "revision": "e486dd4fc81c724f6e4e",
+    "url": "/playBuzz/static/js/2.2b6a3eb9.chunk.js"
+  },
+  {
+    "revision": "e5d52d4f8812e5faf075",
+    "url": "/playBuzz/static/js/main.dcba9769.chunk.js"
+  },
+  {
+    "revision": "ed14d3c1255d60595579",
+    "url": "/playBuzz/static/js/runtime~main.71e769fc.js"
+  }
+]);
